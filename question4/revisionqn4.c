f439a9ac4e049637d66a4ce4d7f720bfc77cda87
Kihiu Allan Njogu <allan.kihiu@students.jkuat.ac.ke>
2024-11-03 17:16:40 +0300

    the code calculates the tax payable by a person depending on two conditions that is: 1.The number of dependants 2.The gross pay,where the gross pay is taxed at different rates depending on the amount. After which the program is set to give out the gross pay ,the net pay and the tax payable

---
+++ question4/revisionqn4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int dependant_number, gross_pay;
+    double net_pay, tax_payable;
+    printf("Please enter your gross pay : ");
+    scanf("\n%d", &gross_pay);
+    printf("\nPlease enter the number of dependants that you have : ");
+    scanf("\n%d", &dependant_number);
+    printf("\nGross pay = %d", gross_pay);
+    if (dependant_number < 3)
+    {
+        if (gross_pay <= 10000)
+        {
+            tax_payable = 0.0 / 100 * gross_pay;
+            printf("\nTax payable = %lf", tax_payable);
+        }
+        else if (gross_pay > 10000 && gross_pay <= 20000)
+        {
+            tax_payable = (15.0 / 100) * gross_pay;
+            printf("\nTax payable = %lf", tax_payable);
+        }
+        else if (gross_pay > 20000)
+        {
+            tax_payable = (35.0 / 100) * gross_pay;
+            printf("\nTax payable = %lf", tax_payable);
+        }
+    }
+    if (dependant_number >= 3)
+    {
+        if (gross_pay <= 10000)
+        {
+            tax_payable = 0.0 / 100 * gross_pay;
+            printf("\nTax payable = %lf", tax_payable);
+        }
+        else if (gross_pay > 10000 && gross_pay <= 20000)
+        {
+            tax_payable = (10.0 / 100) * gross_pay;
+            printf("\nTax payable = %lf", tax_payable);
+        }
+        else if (gross_pay > 20000)
+        {
+            tax_payable = (25.0 / 100) * gross_pay;
+            printf("\nTax payable = %lf", tax_payable);
+        }
+    }
+    net_pay = gross_pay - tax_payable;
+
+    printf("\nNet pay = %lf", net_pay);
+
+    return 0;
+}
