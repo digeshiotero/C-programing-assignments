b54820e530afd6f5bddf55516796ad45d461a9d9
Ian <ianmugi580@gmail.com>
2024-11-04 13:23:04 +0300

    Added comments to make the code more readable

--- question1pt1.c
+++ question1pt1.c
@@ -4,10 +4,12 @@
 int main () 
 {
     int rounds, lucky_number, remainder, count=1, total_score=0;
+    /*Seeding the random number to the current time*/
     srand (time(0));
+    /*Generating a random number between 1 and 100*/
     int secret_number= rand() % 100 + 1;
 
-    printf("Enter the Number of Rounds You Wish to Play: ");
+    printf("Enter The Number Of Rounds You Wish To Play: ");
     scanf("%d", &rounds);
 
     return 0;
