5cecc7da78dec5866b3489a513c798e135d3a7db
Okoth Calvince Omondi <calvinceomondi913@gmail.com>
2024-11-05 16:51:26 +0300

    Update revisionqn4.c

--- question4/revisionqn4.c
+++ question4/revisionqn4.c
@@ -22,30 +22,14 @@ int main()
             tax_payable = (15.0 / 100) * gross_pay;
             printf("\nTax payable = %lf", tax_payable);
         }
-        else if (gross_pay > 20000)
+        else 
         {
             tax_payable = (35.0 / 100) * gross_pay;
             printf("\nTax payable = %lf", tax_payable);
         }
     }
-    if (dependant_number >= 3)
-    {
-        if (gross_pay <= 10000)
-        {
-            tax_payable = 0.0 / 100 * gross_pay;
-            printf("\nTax payable = %lf", tax_payable);
-        }
-        else if (gross_pay > 10000 && gross_pay <= 20000)
-        {
-            tax_payable = (10.0 / 100) * gross_pay;
-            printf("\nTax payable = %lf", tax_payable);
-        }
-        else 
-        {
-            tax_payable = (25.0 / 100) * gross_pay;
-            printf("\nTax payable = %lf", tax_payable);
-        }
-    }
+    
+     
     net_pay = gross_pay - tax_payable;
 
     printf("\nNet pay = %lf", net_pay);
