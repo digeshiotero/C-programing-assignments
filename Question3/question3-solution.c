00a1c5ddf92c8b3121baaffb9ab8863a290de820
ElpisM <elpis8076@gmail.com>
2024-11-06 16:40:25 +0300

    the do while loop for question 3

---
+++ Question3/question3-solution.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+do
+{
+            printf("enter the quantity :\n");
+            scanf("%d" ,&quantity);
+
+            total_price=price*quantity;
+            if(total_price>credit_limit)
+            {
+                printf("sorry you cannot purchase goods worthy such a value on credit\n");
+            }
+
+}
