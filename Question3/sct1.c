4d468af83b156464c997b7a79ab21bb83ecc7295
Shayanony <18th620006@gmail.com>
2024-11-06 16:12:50 -0800

    a portion of question 3 solution
    done

---
+++ Question3/sct1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ int main()
+ {
+     int credit_limit,price,quantity,total_price,n,counter,quantity2;
+     counter=1;
+     printf("enter the number of clients\n");
+     scanf("%d",&n);
+     while(counter<=n)
+        {
+
+            printf("enter your credit limit :\n");
+             scanf("%d",&credit_limit);
+
+            printf("enter price :\n");
+            scanf("%d",&price);
+
