26d424cd4e021b779f43cd8e2363369b8e338288
SelineOwino <selineowino36@gmail.com>
2024-11-08 19:42:53 +0300

    Initialized Variables

--- Question2/mypov/Initializing_variables/main.c
+++ Question2/mypov/Initializing_variables/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    int adm_number, subject1, subject2, subject3;
+    float average_marks;
+    char surname[20];
+    char choice;
     return 0;
 }
