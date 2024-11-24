6657786746d96f503e97f1c41dcff88df30086a1
digeshiotero <otanaadongo@gmail.com>
2024-11-24 15:45:24 +0300

    Question3-solution.c

--- Question3/question3-solution.c
+++ Question3/question3-solution.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int credit_limit, price, quantity, total_price, n, counter, quantity2;
+    counter = 1;
+    printf("Enter number of clients\n");
+    scanf("%d",&n);
+    while(counter <= n){
+
+        printf("Enter your credit limit\n");
+        scanf("%d",&credit_limit);
+
+        printf("Enter price:\n");
+        scanf("%d",&price);
+
+
+        do{
+
+            printf("Enter the quantity:\n");
+            scanf("%d",&quantity);
+
+            total_price = price * quantity;
+            if(total_price > credit_limit){
+                printf("Sorry you cannot purchase goods worth such a value on credit\n");
+            }
+        }while(total_price > credit_limit);
+            printf("Thank you for purchasing from us\n the total was %d\n", &total_price);
+            counter ++;
+
+    }
+    return 0;
+}
 
