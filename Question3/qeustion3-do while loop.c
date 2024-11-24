42b895153a9ff012500792de51b9f9b56d4db764
ElpisM <elpis8076@gmail.com>
2024-11-24 11:24:28 +0300

    question 3 upto the do while section

--- Question3/qeustion3-do while loop.c
+++ Question3/qeustion3-do while loop.c
@@ -1,14 +1,31 @@
-//this is the do while loop in question three 
+#include <stdio.h>
 
-do
+int main ()
 {
-            printf("enter the quantity :\n");
-            scanf("%d" ,&quantity);
+    int credit_limit, price, quantity, total_price, n, counter, quantity2;
+    counter=1;
+    printf("enter number of clients :\n");
+    scanf("%d", &n);
 
-            total_price=price*quantity;
-            if(total_price>credit_limit)
-            {
-                printf("sorry you cannot purchase goods worthy such a value on credit\n");
-            }
+    while(counter<=n)
+    {
+        printf("enter your credit limit :\n");
+        scanf("%d", &credit_limit);
 
+        printf("enter price of the item :\n");
+        scanf("%d", &price);
+        
+        do
+        {
+                    printf("enter the quantity :\n");
+                    scanf("%d" ,&quantity);
+
+                    total_price=price*quantity;
+                    if(total_price>credit_limit)
+                    {
+                        printf("sorry you cannot purchase goods worthy such a value on credit\n");
+                    }
+
+        }
+    }
 }
